8ca720ebcd7e8302fe2ae3d7ebf0e6be939683e6
huongdz2k3 <mr.flooo1230@gmail.com>
2023-04-28 11:42:36 +0700

    customer-service

---
+++ customer/ent/schema/customer.go
@@ -0,0 +1,75 @@
+package schema
+
+import (
+	"entgo.io/contrib/entproto"
+	"entgo.io/ent"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/field"
+	"time"
+)
+
+// Customer holds the schema definition for the Customer entity.
+type Customer struct {
+	ent.Schema
+}
+
+// Fields of the Customer.
+func (Customer) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("name").NotEmpty().MinLen(2).MaxLen(8).
+			Annotations(
+				entproto.Field(2),
+			),
+		field.String("phone_number").NotEmpty().MinLen(10).MaxLen(10).
+			Annotations(
+				entproto.Field(3),
+			),
+		field.String("email").NotEmpty().Unique().
+			Annotations(
+				entproto.Field(4),
+			),
+		field.String("license_id").Unique().MinLen(10).
+			Annotations(
+				entproto.Field(5),
+			),
+		field.String("address").NotEmpty().
+			Annotations(
+				entproto.Field(6),
+			),
+		field.Int("membership_number").NonNegative().Nillable().
+			Annotations(
+				entproto.Field(7),
+			),
+		field.Bool("is_active").Default(true).
+			Annotations(
+				entproto.Field(8),
+			),
+		field.String("password").MinLen(8).NotEmpty().
+			Annotations(
+				entproto.Field(9),
+			),
+		field.Time("create_at").Default(time.Now()).
+			Annotations(
+				entproto.Field(10),
+			),
+		field.Time("update_at").Default(time.Now()).
+			Annotations(
+				entproto.Field(11),
+			),
+		field.String("role").NotEmpty().
+			Annotations(
+				entproto.Field(12),
+			),
+	}
+}
+
+// Edges of the Customer.
+func (Customer) Edges() []ent.Edge {
+	return nil
+}
+
+func (Customer) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entproto.Message(), entproto.Service(),
+	}
+}
---
+++ customer/go.mod
@@ -0,0 +1,36 @@
+module customer-service
+
+go 1.20
+
+require (
+	entgo.io/contrib v0.4.4
+	entgo.io/ent v0.12.2
+	github.com/lib/pq v1.10.9
+	go.uber.org/zap v1.24.0
+	google.golang.org/grpc v1.52.3
+	google.golang.org/protobuf v1.30.0
+)
+
+require (
+	ariga.io/atlas v0.10.1 // indirect
+	github.com/agext/levenshtein v1.2.3 // indirect
+	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/bufbuild/protocompile v0.5.1 // indirect
+	github.com/go-openapi/inflect v0.19.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.16.2 // indirect
+	github.com/jhump/protoreflect v1.15.1 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/zclconf/go-cty v1.13.1 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.8.0 // indirect
+	google.golang.org/genproto v0.0.0-20221118155620-16455021b5e6 // indirect
+)
